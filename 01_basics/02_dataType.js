0e12bfdcab473047b2c7167a7cb388c4c5a87a49
akshar1907 <hiraniakshar@gmail.com>
2025-01-04 10:55:24 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,25 @@
+" use strict"; //treat all js codeas newer version
+
+//alert( 3 + 3) // we are using nodejs not browser
+
+console.log(3 + 3)
+
+console.log("akshar")
+
+
+let name = "akshar"
+let age = 17
+let isLoggedIn = false
+let state;
+
+//string => ""
+//bigint
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
