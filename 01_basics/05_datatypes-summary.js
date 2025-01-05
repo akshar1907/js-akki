8479e5e35a9589c961192bece308f3beae81a0bb
akshar1907 <hiraniakshar@gmail.com>
2025-01-05 17:56:08 +0000

    datatype-summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,37 @@
+// Primitive Datatype
+
+// 7 types : string, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 35677627746647778n
+
+
+
+// Reference type (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+    myObj = {
+        name: "akshar",
+        age: 17,
+    }
+
+    const myFunction = function(){
+        console.log("hello world");
+        
+    }
+    console.log(typeof myFunction);
+    
+
